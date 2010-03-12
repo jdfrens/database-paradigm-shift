459cd5be8246e03d9e9b0c925e05c989a242e97f
Jeremy D. Frens <jdfrens@gmail.com>
2010-03-12 15:25:36 -0600

    using a Report class so extract method is easier; added ownership to output

---
+++ lib/report.rb
@@ -0,0 +1,66 @@
+require 'mongo_reviews'
+require 'utils'
+
+class Report
+  def generate
+    ["book", "movie", "tvshow"].each do |top_genre|
+      header(top_genre)
+      Book.all(:genres => top_genre, :order => "sortabletitle").each do |book|
+        title(book)
+        genres(book)
+        external_identifiers(book)
+      end
+      timestamp
+    end
+  end
+  
+  def textilize_identifier(identifier)
+    case identifier.kind
+    when "imdb"
+      "\"IMDb\":http://www.imdb.com/title/#{identifier.value}"
+    when "isbn"
+      "\"ISBN\":http://books.google.com/books?as_isbn=#{identifier.value}"
+    when "loc"
+      "\"LoC\":http://lccn.loc.gov/#{identifier.value}"
+    end
+  end
+  
+  private
+  def header(top_genre)
+    puts "h1. #{top_genre.pluralize.capitalize}"
+    blank_line
+  end
+  
+  def title(book)
+    ownership = ""
+    if (book.ownerships.length > 0)
+      ownership = " *"
+    end
+    puts "h2. #{book.title}#{ownership}"
+    blank_line
+  end
+  
+  def genres(book)
+    genres = book.genres[1..-1]
+    unless genres.empty?
+      puts "*genres:* #{genres.join(", ")}"
+      blank_line
+    end
+  end
+  
+  def external_identifiers(book)
+    unless book.external_identifiers.empty?
+      print "*external links*: "
+      output = book.external_identifiers.map do |identifier|
+        textilize_identifier(identifier)
+      end.join(", ")
+      puts output
+      blank_line
+    end
+  end
+  
+  def timestamp
+    blank_line
+    puts "Generated at: #{Time.now.to_s}"
+  end
+end
